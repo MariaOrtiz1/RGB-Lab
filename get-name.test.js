420638f458d2d8d390d99e14225f6329e4a34e02
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-07-20 00:51:52 -0700

    first test for get-name

---
+++ get-name.test.js
@@ -0,0 +1,10 @@
+describe('getName', () => {
+  it('returns the name property of an object', () => {
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+    const expected = 'spot';
+    const actual = getName(spot);
+    expect(expected).toEqual(actual);
+  });
+
+});
+
