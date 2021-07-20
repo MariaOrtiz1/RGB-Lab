2977cb50cfb38d63aedc1dc2f63f33f7265bfc80
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-07-20 00:58:02 -0700

    added a get-name.js, and test passed

--- get-name.test.js
+++ get-name.test.js
@@ -1,3 +1,5 @@
+import getName from './get-name.js';
+
 describe('getName', () => {
   it('returns the name property of an object', () => {
     const spot = { name: 'spot', age: 5, weight: '20 lbs' };
